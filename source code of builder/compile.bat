36e49a8b1d85f1667588bfb00c8d0e81a4ccd5da
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-04-18 23:18:55 +0300

    Delete source code of builder directory

--- source code of builder/compile.bat
+++
@@ -1,6 +0,0 @@
-nuitka --windows-disable-console --onefile --follow-imports --enable-plugin=tk-inter --remove-output --windows-icon-from-ico=icon.ico TheMurkBuilder.py
-
-rmdir /s /q build
-
-:cmd
-pause null
