41de3fc10246c3c768a0fd9b89b257856923dc39
Nick Vinesmoke <darkdedpro@gmail.com>
2023-12-22 15:53:23 +0200

    fixed packer

--- builder/pack.bat
+++ builder/pack.bat
@@ -1,4 +1,4 @@
-pyinstaller --noconfirm --onefile --icon "icon.ico" --name "TheMurkBuilder" --upx-dir "../upx-4.2.1-win64" --version-file "version.py" --add-data "stub.dll;."  "TheMurkBuilder.py"
+pyinstaller --noconfirm --onefile --icon "icon.ico" --name "TheMurkBuilder" --upx-dir "../upx-4.2.1-win64" --version-file "version.py" "TheMurkBuilder.py"
 
 rmdir /s /q __pycache__
 rmdir /s /q build
