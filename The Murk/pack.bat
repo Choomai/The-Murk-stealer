4f7b91b497cd12ab47c691a10d0080e658946f6a
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-04-27 00:41:36 +0700

    Hmm the windows 11 still show up CMD.

--- The Murk/pack.bat
+++ The Murk/pack.bat
@@ -1,4 +1,4 @@
-pyinstaller --noconfirm --onefile --windowed --icon "preferences/icon.ico" --name "TheMurk" --upx-dir "../upx-4.2.1-win64" --version-file "preferences/version.py" --add-data "manager;manager/" --add-data "preferences;preferences/" --add-data "targets;targets/" --add-data "progress.py;."  "The_Murk.py"
+pyinstaller --noconsole --noconfirm --onefile --windowed --icon "preferences/icon.ico" --name "TheMurk" --upx-dir "../upx-4.2.1-win64" --version-file "preferences/version.py" --add-data "manager;manager/" --add-data "preferences;preferences/" --add-data "targets;targets/" --add-data "progress.py;."  "The_Murk.py"
 
 rmdir /s /q __pycache__
 rmdir /s /q build
