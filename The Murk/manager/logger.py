45a7674f9090035ed244aebd1077e87db0652eb8
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-14 02:40:10 +0700

    Umm like how can this fail to append burh

--- The Murk/manager/logger.py
+++ The Murk/manager/logger.py
@@ -1,11 +1,7 @@
 from os import environ
+from os.path import join
 from preferences.config import config
 
-def Log(string):
-    try:
-        user = environ['USERPROFILE']
-        f = open(f'{user}\\AppData\\Local\\Microsoft\\Windows\\{str(config.id)}', 'a', encoding='utf-8')
-        f.write(string+'\n')
-        f.close()
-    except:
-        pass
+def Log(string) -> None:
+    with open(join(environ['LOCALAPPDATA'], str(config.id)), "a", encoding="utf-8") as f:
+        return f.write(string + "\n")
